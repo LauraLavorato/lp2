4cd69aa08f21b8b53743998eeca92bb35d75ffab
LauraLavorato <laura.lavorato@hotmail.com>
2015-03-16 10:14:31 -0300

    Create ProgramaJava1.java
    Meu primeiro programa em java no terminal

---
+++ aulas/ProgramaJava1.java
@@ -0,0 +1,7 @@
+public class ProgramaJava1{
+	public static void main(String [] args){
+
+		/*Imprime no console a frase: Meu primeiro programa em java */
+		System.out.println("Meu primeiro programa em java");
+	}
+}
